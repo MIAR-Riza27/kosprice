aec76ff043b11aa8ec0c15388a7f54019d076578
MIAR-Riza27 <ipnualvariza@gmail.com>
2025-08-18 10:32:08 +0700

    feat: Add dummy frontend (HTML + fetch API)

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from model import KosPriceModel
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 model = KosPriceModel()
+
 
 class KosInput(BaseModel):
     luas_m2: float
     jumlah_kamar: int
     jarak_kampus_km: float
+
 
 @app.post("/predict")
 def predict_price(data: KosInput):
@@ -16,7 +26,7 @@ def predict_price(data: KosInput):
         result = model.predict(
             luas_m2=data.luas_m2,
             jumlah_kamar=data.jumlah_kamar,
-            jarak_kampus_km=data.jarak_kampus_km
+            jarak_kampus_km=data.jarak_kampus_km,
         )
         return {"prediksi_harga": result}
     except Exception as e:
