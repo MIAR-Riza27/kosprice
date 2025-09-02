0ddf04a5afe96b2698006b4b090f0960ed26bd4f
MIAR-Riza27 <ipnualvariza@gmail.com>
2025-09-02 14:37:27 +0700

    feat: scraper scroll & load listing page

--- regions.py
+++ regions.py
@@ -1,1 +1,9 @@
-regions = [""]
+regions = [
+    "bandung-kota-bandung-jawa-barat-indonesia",
+    "semarang-kota-semarang-jawa-tengah-indonesia",
+    "surabaya-kota-surabaya-jawa-timur-indonesia",
+    "yogyakarta-kota-yogyakarta-daerah-istimewa-yogyakarta-indonesia",
+    "jakarta-selatan-kota-jakarta-selatan-dki-jakarta-indonesia",
+    "malang-kota-malang-jawa-timur-indonesia",
+    "solo-kota-surakarta-jawa-tengah-indonesia",
+]
--- scrape.py
+++ scrape.py
@@ -1,10 +1,12 @@
 from playwright.sync_api import sync_playwright
 import time
 
+from regions import regions
+
 
 def scrape(playwright):
     browser = playwright.chromium.launch_persistent_context(
-        user_data_dir="C:\playwright",
+        user_data_dir=r"C:\playwright",
         channel="chrome",
         headless=False,
         no_viewport=True,
@@ -12,6 +14,35 @@ def scrape(playwright):
 
     page = browser.new_page()
 
-    page.goto(
-        f"https://mamikos.com/cari/semarang-kota-semarang-jawa-tengah-indonesia/all/bulanan/0-15000000/158?keyword=semarang&suggestion_type=search&rent=2&sort=price,-&price=10000-20000000&singgahsini=0"
-    )
+    for region in regions:
+        region_keywords = region.split("-")[0]
+        page.goto(
+            f"https://mamikos.com/cari/{region}/all/bulanan/0-15000000/158"
+            f"?keyword={region_keywords}&suggestion_type=search&rent=2&sort=price,-&price=10000-20000000"
+        )
+
+        for more_rooms in range(3):
+            page.wait_for_selector("button.nominatim-list__see-more")
+
+            button = page.get_by_role(
+                "button", name="Lihat lebih banyak lagi", exact=False
+            )
+            button.scroll_into_view_if_needed()
+            button.click()
+            time.sleep(1)
+
+        # Example: Interact with the element using Playwright instead of getEventListeners
+        # You can extract attributes or perform actions as needed, e.g.:
+        kost_rc_element = page.query_selector(".kost-rc")
+        # Do something with kost_rc_element, e.g., print its text content
+        if kost_rc_element:
+            print(kost_rc_element.text_content())
+
+        time.sleep(2)
+
+    browser.close()
+
+
+if __name__ == "__main__":
+    with sync_playwright() as playwright:
+        scrape(playwright)
