523d4fc31a8f9e6215b827a36e9ea5f505240e4b
MIAR-Riza27 <ipnualvariza@gmail.com>
2025-09-05 09:35:08 +0700

    feat: Add backup sistem, skip already scraped regions for scraper

--- regions.py
+++ regions.py
@@ -1,12 +1,23 @@
 regions = [
-    "bandung-city-jawa-barat-indonesia",
-    "semarang-city-jawa-tengah-indonesia",
-    "surabaya-city-jawa-timur-indonesia",
-    "yogyakarta-city-daerah-istimewa-yogyakarta-indonesia",
-    "jakarta-city-dki-jakarta-indonesia",
-    "malang-city-jawa-timur-indonesia",
-    "solo-city-jawa-tengah-indonesia",
-    "jember-city-jawa-timur-indonesia",
-    "denpasar-city-bali-indonesia",
-    "makassar-city-sulawesi-selatan-indonesia",
+    # Kota Besar (Metropolitan)
+    "jakarta-daerah-khusus-ibukota-jakarta-indonesia",
+    "bandung-kota-bandung-jawa-barat-indonesia",
+    "surabaya-kota-surabaya-jawa-timur-indonesia",
+    "semarang-kota-semarang-jawa-tengah-indonesia",
+    "medan-kota-medan-sumatera-utara-indonesia",
+    "makasar-makasar-kota-jakarta-timur-daerah-khusus-ibukota-jakarta-indonesia",
+    # Kota Pelajar
+    "yogyakarta-kota-yogyakarta-daerah-istimewa-yogyakarta-indonesia",
+    "malang-kota-malang-jawa-timur-indonesia",
+    "solo-kota-surakarta-jawa-tengah-indonesia",
+    "kota-denpasar-bali-indonesia",
+    "depok-kota-depok-jawa-barat-indonesia",
+    "bogor-bogor-west-java-indonesia",
+    # Kota Tambahan (opsional, kalau scraping lebih luas)
+    "bekasi-jawa-barat-indonesia",
+    "tangerang-kota-tangerang-banten-indonesia",
+    "balikpapan-balikpapan-kota-balikpapan-kalimantan-timur-indonesia",
+    "pontianak-kota-pontianak-kalimantan-barat-indonesia",
+    "palembang-palembang-kota-palembang-sumatera-selatan-indonesia",
+    "batam-kota-batam-kepulauan-riau-indonesia",
 ]
